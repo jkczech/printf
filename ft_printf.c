fe9c8d630a6cddfc04d655a9e3daabc42ecb2230
Josef Koupy <jkoupy@c3r4p6.42vienna.com>
2023-09-19 14:49:30 +0200

    first

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 14:44:22 by jkoupy            #+#    #+#             */
+/*   Updated: 2023/09/19 14:47:24 by jkoupy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *format, ...)
 {
-	while
+	int	i;
+
+	i = 0;
+	while (format[i])
+	{
+		write(1, &format[i], 1);
+		i++;
+	}
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (1);
+	ft_printf(argv[1]);
+	return (0);
 }
